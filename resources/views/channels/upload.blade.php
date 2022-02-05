659d9ec32455fd94c78e99776ceaf4a04943ba02
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2022-02-05 15:46:31 +0900

    Intermediate HTML for progress bar

--- resources/views/channels/upload.blade.php
+++ resources/views/channels/upload.blade.php
@@ -11,8 +11,24 @@
                     <p class="text-ceter">Upload Videos</p>
                 </div>
                 <div class="card p-3" v-else>
-            </div>
-
+                    <div class="my-4">
+                        <div class="progress mb-3">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemin="100"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="d-flex justify-content-center align-items-center" style="height: 180px; color: white; font-size: 18px; background: #808080;">
+                                        Loading thumbnail ...
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="text-center">
+                                    My Awesome video
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </channel-uploads>
     </div>

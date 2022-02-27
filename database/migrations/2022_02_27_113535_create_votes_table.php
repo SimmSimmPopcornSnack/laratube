b587641a7d676e83ae0b98f971fc6a6f0077c949
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2022-02-27 21:52:20 +0900

    Done 30. Polymorphic votes

---
+++ database/migrations/2022_02_27_113535_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->uuid("id");
+            // $table->bigIncrements('id');
+            $table->enum("type", ["up", "down"])->default("up");
+            // $table->morphs("voteable");
+            $table->string("voteable_type");
+            $table->uuid("voteable_id");
+            $table->uuid("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}

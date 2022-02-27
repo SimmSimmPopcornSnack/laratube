29fb596017dcba791e2c2794ec8505a6f727cec6
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2022-02-28 03:20:41 +0900

    Done 31. Reusable votes component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 window.Vue = require('vue').default;
 Vue.config.ignoredElements = ["video-js"];
 Vue.component("subscribe-button", require("./components/subscribe-button.vue").default);
+Vue.component("votes", require("./components/votes.vue").default);
 require("./components/channel-uploads").default;
 const app = new Vue({
     el: '#app',
